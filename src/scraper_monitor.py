bd87653e5e2b6f282ab199265a8e8303f8637819
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-09-18 20:47:41 +0200

    Add full type hints to scraper_monitor
    Callable argument and return types were missing.

--- src/scraper_monitor.py
+++ src/scraper_monitor.py
@@ -7,7 +7,7 @@ from updates import get_updates, select_new_updates
 FLOOD_PREVENTION_DELAY_SECONDS = 2
 
 
-def scraper_monitor(update_sender: Callable) -> None:
+def scraper_monitor(update_sender: Callable[[str, str], None]) -> None:
     updates = get_updates()
     new_updates = select_new_updates(updates, get_latest_id())
     for update in new_updates:
@@ -15,6 +15,8 @@ def scraper_monitor(update_sender: Callable) -> None:
         send_update(update_sender, update.user, update.content)
 
 
-def send_update(update_sender: Callable, user: str, content: str) -> None:
+def send_update(
+    update_sender: Callable[[str, str], None], user: str, content: str
+) -> None:
     update_sender(user, content)
     sleep(FLOOD_PREVENTION_DELAY_SECONDS)  # Dirty anti-flood prevention
