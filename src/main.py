cb8cfaba0780bcfc2c7de63ba38f51f5ea15e32f
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-09-18 22:52:28 +0200

    Exit program when no valid config was provided
    Exit program when neither Telegram nor Discord configuration was provided.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from os import environ
 from time import sleep
 
+from loguru import logger
 from requests import get, post
 
 from history import get_latest_id, store_latest_id
@@ -12,18 +13,27 @@ FLOOD_PREVENTION_DELAY_SECONDS = 2
 TELEGRAM_API_URL = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}"
 BOT_TOKEN = environ.get("BOT_TOKEN")
 GROUPS = environ.get("GROUPS")
+VALID_TELEGRAM_CONFIG = BOT_TOKEN and GROUPS
 
 WEBBOOK_URL = environ.get("WEBHOOK_URL")
 AVATAR_URL = environ.get("AVATAR_URL")
+VALID_DISCORD_CONFIG = WEBBOOK_URL is not None
 
 
 def main() -> None:
+    if not VALID_TELEGRAM_CONFIG and not VALID_DISCORD_CONFIG:
+        logger.error("Not valid config for either Telegram nor Discord!")
+        return
     while not sleep(MSG_DELAY):
-        updates = get_updates()
-        new_updates = select_new_updates(updates, get_latest_id())
-        for update in new_updates:
-            store_latest_id(update.id)
-            send_update(update.user, update.content)
+        scraper_monitor()
+
+
+def scraper_monitor() -> None:
+    updates = get_updates()
+    new_updates = select_new_updates(updates, get_latest_id())
+    for update in new_updates:
+        store_latest_id(update.id)
+        send_update(update.user, update.content)
 
 
 def send_update(user: str, content: str) -> None:
