8cd6c9b449d8cfa7a1f3b6935b6c1f120cfb1696
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-09-18 20:42:21 +0200

    Extract common logic from both mains

--- src/main_telegram.py
+++ src/main_telegram.py
@@ -1,36 +1,28 @@
 from os import environ
-from time import sleep
 
 from telegram.ext import CallbackContext, Updater
 
-from history import get_latest_id, store_latest_id
-from updates import get_updates, select_new_updates
+from scraper_monitor import scraper_monitor
 
 BOT_TOKEN = environ["BOT_TOKEN"]
 MSG_DELAY = int(environ["MSG_DELAY"])
 GROUPS = environ["GROUPS"].split(",")
-FLOOD_PREVENTION_DELAY_SECONDS = 2
 
 
 def main() -> None:
     updater = Updater(token=BOT_TOKEN, use_context=True)
-    updater.job_queue.run_repeating(scraper_monitor, MSG_DELAY)
+    updater.job_queue.run_repeating(monitor_job, MSG_DELAY)
     updater.start_polling()
     updater.idle()
 
 
-def scraper_monitor(context: CallbackContext) -> None:
-    updates = get_updates()
-    new_updates = select_new_updates(updates, get_latest_id())
-    for update in new_updates:
-        store_latest_id(update.id)
-        send_update(context, update.user, update.content)
+def monitor_job(context: CallbackContext) -> None:
+    scraper_monitor(lambda user, content: send_update(context, user, content))
 
 
 def send_update(context: CallbackContext, user: str, content: str) -> None:
     for group in GROUPS:
         context.bot.send_message(group, f"{user}: {content}")
-    sleep(FLOOD_PREVENTION_DELAY_SECONDS)  # Dirty anti-flood prevention
 
 
 if __name__ == "__main__":
